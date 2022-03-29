24cd3511f288b9982640a076b2cc8ad4a75dd0c7
Lwalsh21 <97565562+Lwalsh21@users.noreply.github.com>
2022-03-29 17:06:27 -0400

    finished

--- Global.gd
+++ Global.gd
@@ -1,5 +1,26 @@
 extends Node
 
+func _ready():
+	pause_mode = PAUSE_MODE_PROCESS		# global should never be paused
+
 func _unhandled_input(event):
-	if event.is_action_pressed("menu"):
-		get_tree().quit()
+	if event.is_action_pressed("menu"):	# instead of quitting, show the menu
+		#get_tree().quit()
+		var menu = get_node_or_null("/root/Game/Menu")
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true 	# pause the game while the menu is visible
+			else:
+				menu.hide()
+				get_tree().paused = false
+Then add the following functionality to res://UI/Menu.gd:
+
+extends Control
+
+func _on_Restart_pressed():				# if we restart, then unpause the game and the reload the scene
+	get_tree().paused = false
+	get_tree().change_scene("res://Maze/Maze.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
